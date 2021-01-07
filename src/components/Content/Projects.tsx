ae4de5a81fcf3d9fa87cf426070b470ee7938917
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2021-01-07 22:45:54 +0100

    update the master

--- src/components/Content/Projects.tsx
+++ src/components/Content/Projects.tsx
@@ -1,31 +1,34 @@
 import React, { useState } from "react";
 import { useProjectsValue, useSelectedProjectValue } from "../../context";
 
-export const Project = ({ activeValue = null}) => {
+export const Projects = ({ activeValue = null}) => {
   const [active, setActive] = useState<string | null>(activeValue);
   const  { setSelectedProjecthandler } = useSelectedProjectValue();
    const  { projects } = useProjectsValue();
 
 
-   return (
-       projects && projects.map(project => (
-           <li 
-              key={project.projectId}
-              data-doc-id={project.docId}
-              data-testid="project-action"
-              className={
-                  active === project.projectId 
-                  ?
-                  'active sidebar__project'
-                  : 'sidebar__project'
-              }
-              onClick={() => {
-                  setActive('');
-                  setSelectedProjecthandler('');
-              }}
-             >
+   const allproject = (projects && projects.map(project => {
+                return (<li 
+                        key={project.projectId}
+                        data-doc-id={project.docId}
+                        data-testid="project-action"
+                        className={
+                            active === project.projectId 
+                            ?
+                            'active sidebar__project'
+                            : 'sidebar__project'
+                        }
+                        onClick={() => {
+                            setActive('');
+                            setSelectedProjecthandler('');
+                        }}>
+                         I am a Project  
+                    </li>)
+                }));
 
-           </li>
-       ))
+   return (
+       <ul>
+           {allproject}
+       </ul>
    )
 }
